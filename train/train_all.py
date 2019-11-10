ce330e006e2b8697b2f3300530b29302d1b5ed6d
Smit <sthakkar1407@gmail.com>
2019-11-09 23:46:31 -0800

    1. num_val 20

--- train/train_all.py
+++ train/train_all.py
@@ -37,7 +37,7 @@ parser.add_argument('--input_ques_h5', default='../script/data/visdial_data_demo
 parser.add_argument('--input_json', default='../script/data/visdial_params_demo.json', help='path to dataset, now hdf5 file')
 parser.add_argument('--outf', default='./save', help='folder to output images and model checkpoints')
 parser.add_argument('--encoder', default='scrach', help='what encoder to use.')
-parser.add_argument('--num_val', default=1000, help='number of image split out as validation set.')
+parser.add_argument('--num_val', default=20, help='number of image split out as validation set.')
 parser.add_argument('--update_D', action='store_true', help='whether train use the GAN loss.')
 parser.add_argument('--update_LM', action='store_true', help='whether train use the GAN loss.')
 
