398488e8ae9281ec53c4698a1c3cbaf7e1b7a54b
Smit <sthakkar1407@gmail.com>
2019-11-16 15:21:55 -0800

    everything inside function --> test_data.py

--- script/test_data.py
+++ script/test_data.py
@@ -9,19 +9,20 @@ import argparse
 # opt = parser.parse_args()
 # path = opt.path
 
-path = '../script/data/vdl_img_vgg.h5'
-f = h5py.File(path, 'r')
-train_feats = f['images_train']
-val_feats = f['images_val']
 
-idx_list = [0,5,50,4335,2134]
-
-actual_train_ans = [2.7524679,1.5914612,2.9992323,2.7822866,2.0583184]
-actual_val_ans = [2.4914272,3.1998599,1.7559544,1.7371163,1.766684]
-
-error = 0
-tolerance = 1e-4
 def check_data():
+    path = '../script/data/vdl_img_vgg.h5'
+    f = h5py.File(path, 'r')
+    train_feats = f['images_train']
+    val_feats = f['images_val']
+
+    idx_list = [0, 5, 50, 4335, 2134]
+
+    actual_train_ans = [2.7524679, 1.5914612, 2.9992323, 2.7822866, 2.0583184]
+    actual_val_ans = [2.4914272, 3.1998599, 1.7559544, 1.7371163, 1.766684]
+
+    error = 0
+    tolerance = 1e-4
 
     print('training features...')
     idx = 0
