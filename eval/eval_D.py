e95c43c6b80a56ba95ee6bc5d7b174e8209e9a24
Smit <sthakkar1407@gmail.com>
2019-11-19 12:21:02 -0800

    json path from the model path given

--- eval/eval_D.py
+++ eval/eval_D.py
@@ -37,9 +37,17 @@ parser.add_argument('--input_json', default='visdial_params.json', help='visdial
 parser.add_argument('--model_path', default='', help='folder to output images and model checkpoints')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--log_iter', type=int, default=1)
+parser.add_argument('--path_to_home',type=str)
 
 opt = parser.parse_args()
 sys.path.insert(1, opt.path_to_home)
+
+# json output path
+pth = os.path.split(opt.model_path)
+tail = pth[1]
+tail2 = tail[:-4]
+json_path = tail2+'.json'
+print('output will be dumped to: '+ json_path )
 
 if(opt.model_path==''):
     print('Model path required for evaluation')
@@ -51,7 +59,6 @@ if opt.cuda:
 print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
-
 
 from misc.utils import repackage_hidden_new, clip_gradient, adjust_learning_rate, decode_txt, get_eval_logger
 import misc.dataLoader as dl
@@ -313,4 +320,4 @@ mrr = np.sum(1/(np.array(rank_all, dtype='float'))) / float(len(rank_all))
 logger.warning('Final result: ')
 logger.warning('%d/%d: mrr: %f R1: %f R5 %f R10 %f Mean %f' %(1, len(dataloader_val), mrr, R1, R5, R10, ave))
 print(result_all)
-json.dump(result_all, open('top_10_disc.json', 'w'))
+json.dump(result_all, open(json_path, 'w'))
