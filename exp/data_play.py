cb511222c9a6d14f48771b0d092730a36245135a
yashphogat <yashnitd@gmail.com>
2019-11-28 14:27:49 -0800

    added dataplay

--- exp/data_play.py
+++ exp/data_play.py
@@ -1,1 +1,9 @@
 import torch
+import nltk
+
+from nltk.parse.corenlp import CoreNLPDependencyParser
+dep_parser = CoreNLPDependencyParser(url='http://localhost:9000')
+parse, = dep_parser.raw_parse('What is the first Indian text to introduce iron?')
+print(parse.nodes[0])
+# parse = dep_parser.tag('The quick brown fox jumps over the lazy dog.'.split())
+print(parse.to_conll(10))
